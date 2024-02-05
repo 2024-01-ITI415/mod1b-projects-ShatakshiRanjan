88de0f08535014a3ac1f446ca6199bd1b6955080
ShatakshiRanjan <shatakshiranjan02@gmail.com>
2024-02-05 10:12:52 -0500

    Highscore - bugfix

--- Assets/01-Apple Picker/Scripts/HighScore.cs
+++ Assets/01-Apple Picker/Scripts/HighScore.cs
@@ -7,10 +7,22 @@ public class HighScore : MonoBehaviour
 {
     static public int score = 1000;
 
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        PlayerPrefs.SetInt("HighScore", score);
+    }
     // Update is called once per frame
     void Update()
     {
         Text gt = this.GetComponent<Text>();
         gt.text = "HighScore: " + score;
+        if( score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 }
