e30ddffd2997ffe6ed0806b329cf7ac83bafc07e
ShatakshiRanjan <shatakshiranjan02@gmail.com>
2024-02-05 15:02:39 -0500

    launchpad deisgn

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launchPoint;
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+    void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+         launchPoint.SetActive(false);
+    }
+}
